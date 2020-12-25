ea17b44e9638a8483345630f28bd524d6b3a987d
MD ABIR HASAN MUNNA <38092445+ahmunnaah@users.noreply.github.com>
2020-12-25 22:47:42 +0600

    b

--- admin_adder.php
+++ admin_adder.php
@@ -4,7 +4,7 @@ include_once 'db.php';
 $uname = "munna";
 $pass = "ahmunna2222";
 $de_pass = password_hash($pass, PASSWORD_DEFAULT);
-echo "bal";
+
 $sql = "INSERT INTO admin(uname, pass) VALUES ('$uname','$de_pass');";
 mysqli_query($db, $sql);
  ?>
