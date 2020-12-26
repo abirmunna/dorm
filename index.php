725cbff949801df4a81a2b43d9d5007aeb706c19
MD ABIR HASAN MUNNA <38092445+ahmunnaah@users.noreply.github.com>
2020-12-26 22:28:42 +0600

    lil update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="home.css" type="text/css">
   </head>
   <body>
-    <h1>Welcome to bup dorm website.</h1>
+    <h1>Welcome to the Dormitory Management Portal</h1>
     <h2>Login in order to continue.</h2>
     <center><div class="button">
       <a href="admin_login.php" class="login">Admin Login</a>
