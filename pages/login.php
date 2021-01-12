4af1f9f941855bd44052d05dc2bf508c74115f76
MD ABIR HASAN MUNNA <38092445+ahmunnaah@users.noreply.github.com>
2021-01-13 01:46:16 +0600

    new files added

---
+++ pages/login.php
@@ -0,0 +1,24 @@
+
+ <html>
+   <head>
+     <title>Registration</title>
+     <link rel="stylesheet" href="../css/registration.css">
+   </head>
+   <body>
+     <div class="regform"><br><h1>Login</h1>
+       <div class="main">
+         <form action="../handler/login_handler.php" method="POST">
+
+          <h2 class="name">Username :</h2>
+          <input class="username" type="text" name="username" placeholder="Enter a username">
+
+           <h2 class="name">Password :</h2>
+           <input class="pass_1" type="password" name="pass_1" placeholder="Enter your password">
+           <br>
+           <button type="submit" name="login">Sign In</button>
+         </form>
+       </div>
+     </div>
+
+   </body>
+ </html>
