0b855ac20dcf2ead1515cb8ed28873bf39ba9d21
imranhei <imranhossainrana.2014@gmail.com>
2021-01-15 01:58:46 +0600

    design welcome page

---
+++ pages/welcome_std.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Somehow I got an error, so I comment the title, just uncomment to show -->
+    <!-- <title>Animated Sidebar Menu | CodingLab</title> -->
+    <link rel="stylesheet" href="../css/welcome_std.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <div class="wrapper">
+      <input type="checkbox" id="btn" hidden>
+      <label for="btn" class="menu-btn">
+        <i class="fas fa-bars"></i>
+        <i class="fas fa-times"></i>
+      </label>
+      <nav id="sidebar">
+        <div class="title">
+Side Menu</div>
+<ul class="list-items">
+<li><a href="welcome_std.php"><i class="fas fa-home"></i>Home</a></li>
+<li><a href="#"><i class="fas fa-bell"></i>Notification</a></li>
+<li><a href="#"><i class="fas fa-address-book"></i>Meal Menu</a></li>
+<li><a href="#"><i class="fas fa-utensils"></i>Meal</a></li>
+<li><a href="#"><i class="fas fa-money-check"></i>Fees</a></li>
+<li><a href="#"><i class="fas fa-exclamation-circle"></i>Complaints</a></li>
+<li><a href="#"><i class="fas fa-phone-square-alt"></i>Emergency Contucts</a></li>
+<li><a href="admin/welcome.php"><i class="fas fa-unlock-alt"></i>Reset Password</a></li>
+<div class="icons">
+      <!--      <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-github"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>  -->
+          </div>
+</ul>
+</nav>
+    </div>
+<!--  <div class="content">
+      <div class="header">
+Animated Side Navigation Menu</div>
+<p>
+using only HTML and CSS</p> -->
+</div>
+</body>
+</html>
