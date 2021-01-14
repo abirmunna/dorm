731d4e0e62a96d3a4dfc2b5627d94c7835551346
imranhei <imranhossainrana.2014@gmail.com>
2021-01-14 21:44:37 +0600

    new sql

--- db/dorm.sql
+++ db/dorm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2021 at 07:52 PM
+-- Generation Time: Jan 14, 2021 at 04:42 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -45,6 +45,13 @@ CREATE TABLE `building_info` (
   `capacity` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `building_info`
+--
+
+INSERT INTO `building_info` (`room_no`, `capacity`, `status`) VALUES
+('501', '2', '1');
 
 -- --------------------------------------------------------
 
@@ -121,8 +128,41 @@ CREATE TABLE `menu` (
 
 CREATE TABLE `notification` (
   `std_id` varchar(255) NOT NULL,
-  `msg` varchar(455) NOT NULL
+  `msg` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `notification`
+--
+
+INSERT INTO `notification` (`std_id`, `msg`) VALUES
+('18511071', 'Very good student :)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `std_id` int(20) NOT NULL,
+  `std_name` varchar(255) NOT NULL,
+  `std_email` varchar(255) NOT NULL,
+  `std_year` int(20) NOT NULL,
+  `std_dept` varchar(255) NOT NULL,
+  `std_address` varchar(255) NOT NULL,
+  `std_contact` int(20) NOT NULL,
+  `std_blood` varchar(255) NOT NULL,
+  `std_gname` varchar(255) NOT NULL,
+  `std_gcontact` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`std_id`, `std_name`, `std_email`, `std_year`, `std_dept`, `std_address`, `std_contact`, `std_blood`, `std_gname`, `std_gcontact`) VALUES
+(18511091, 'Munna', 'abirmunna091@gmail.com', 3, 'ict', 'airport', 17658467, 'B+', 'rafiq', 171764125);
 
 -- --------------------------------------------------------
 
@@ -162,6 +202,7 @@ CREATE TABLE `staff` (
 CREATE TABLE `student` (
   `std_id` varchar(255) NOT NULL,
   `std_name` varchar(255) NOT NULL,
+  `std_email` varchar(255) NOT NULL,
   `std_year` varchar(255) NOT NULL,
   `std_dept` varchar(255) NOT NULL,
   `std_address` varchar(255) NOT NULL,
@@ -171,6 +212,13 @@ CREATE TABLE `student` (
   `std_gcontact` varchar(255) NOT NULL,
   `room_no` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`std_id`, `std_name`, `std_email`, `std_year`, `std_dept`, `std_address`, `std_contact`, `std_blood`, `std_gname`, `std_gcontact`, `room_no`) VALUES
+('18511071', 'Imran Hossain', 'imranhossainrana.2014@gmail.com', '3rd', 'ICT', 'Khilkhet', '0146646446', 'O+', 'moazzem', '01546465489', '501');
 
 --
 -- Indexes for dumped tables
@@ -199,7 +247,8 @@ ALTER TABLE `complaints`
 -- Indexes for table `fee`
 --
 ALTER TABLE `fee`
-  ADD PRIMARY KEY (`fee_id`);
+  ADD PRIMARY KEY (`fee_id`),
+  ADD KEY `std_id` (`std_id`);
 
 --
 -- Indexes for table `login`
@@ -226,25 +275,35 @@ ALTER TABLE `menu`
 -- Indexes for table `notification`
 --
 ALTER TABLE `notification`
-  ADD UNIQUE KEY `std_id` (`std_id`);
+  ADD UNIQUE KEY `std_id` (`std_id`),
+  ADD KEY `std_id_2` (`std_id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`std_id`);
 
 --
 -- Indexes for table `salary`
 --
 ALTER TABLE `salary`
-  ADD PRIMARY KEY (`salary_id`);
+  ADD PRIMARY KEY (`salary_id`),
+  ADD KEY `stf_id` (`stf_id`);
 
 --
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
-  ADD PRIMARY KEY (`stf_id`);
+  ADD PRIMARY KEY (`stf_id`),
+  ADD KEY `room_no` (`room_no`);
 
 --
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-  ADD PRIMARY KEY (`std_id`);
+  ADD PRIMARY KEY (`std_id`),
+  ADD KEY `room_no` (`room_no`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -267,6 +326,59 @@ ALTER TABLE `fee`
 --
 ALTER TABLE `salary`
   MODIFY `salary_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD CONSTRAINT `complaints_ibfk_1` FOREIGN KEY (`std_id`) REFERENCES `student` (`std_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `fee`
+--
+ALTER TABLE `fee`
+  ADD CONSTRAINT `fee_ibfk_1` FOREIGN KEY (`std_id`) REFERENCES `student` (`std_id`);
+
+--
+-- Constraints for table `login`
+--
+ALTER TABLE `login`
+  ADD CONSTRAINT `login_ibfk_1` FOREIGN KEY (`std_id`) REFERENCES `student` (`std_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `login_ibfk_2` FOREIGN KEY (`stf_id`) REFERENCES `staff` (`stf_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `meal`
+--
+ALTER TABLE `meal`
+  ADD CONSTRAINT `meal_ibfk_1` FOREIGN KEY (`std_id`) REFERENCES `student` (`std_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notification`
+--
+ALTER TABLE `notification`
+  ADD CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`std_id`) REFERENCES `student` (`std_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `salary`
+--
+ALTER TABLE `salary`
+  ADD CONSTRAINT `salary_ibfk_1` FOREIGN KEY (`stf_id`) REFERENCES `staff` (`stf_id`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`room_no`) REFERENCES `building_info` (`room_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`room_no`) REFERENCES `building_info` (`room_no`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
