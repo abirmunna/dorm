3d88dab749063f96311c6b936fe24368baa23e5e
MD ABIR HASAN MUNNA <38092445+ahmunnaah@users.noreply.github.com>
2021-01-16 01:36:05 +0600

    fixed email function

--- handler/forget_handler.php
+++ handler/forget_handler.php
@@ -1,25 +1,23 @@
 <?php
-include_once '../db/db.php';
-session_start();
+include "../db/db.php";
 $id = mysqli_real_escape_string($db, $_POST['std_id']);
 $gmail = mysqli_real_escape_string($db, $_POST['std_email']);
 
 $sql = "SELECT * FROM login WHERE std_id = '$id';";
 $data = mysqli_query($db, $sql);
 $row = mysqli_fetch_array($data);
+
 if ($row['email'] == $gmail) {
   $to_email = $gmail;
   $subject = "Reset password for you dorm web application";
-  $body = "your password is = " . $row['std_pass'];
+  $body = $row['std_pass'];
   $headers = "From: dormitorymanagement719091@gmail.com";
 
   if (mail($to_email, $subject, $body, $headers)) {
       header("location:../pages/forget.php?q=email sent successfully.");
   } else {
-      header("location:../pages/forget.php?q=Invalid email ID.");
+      header("location:../pages/forget.php?q=mail sent error");
   }
 }
-else{
-  header("location:../pages/forget.php?q=Invalid email ID.");
-}
+
 ?>
