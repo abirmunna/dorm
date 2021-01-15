14578e74a5248e4572c552dce0d89710002cddd2
MD ABIR HASAN MUNNA <38092445+ahmunnaah@users.noreply.github.com>
2021-01-16 04:38:58 +0600

    shush removed

--- handler/login_handler.php
+++ handler/login_handler.php
@@ -27,33 +27,7 @@ if(strlen($id) == 4)
   else {
       header("location: ../pages/login.php?q=invalid");
   }
-
-
 }
-
-
-else if (strlen($id)==1) {
-  $sql = "SELECT stf_id,stf_pass FROM login WHERE stf_id = '$id';";
-  $data = mysqli_query($db, $sql);
-  $check = mysqli_num_rows($data);
-
-  if ($check > 0) {
-    $get = mysqli_fetch_assoc($data);
-
-    if($id === $get['stf_id'] && $pass === $get['stf_pass'])
-    {
-      $_SESSION['id'] = $get['stf_id'];
-        header("location: ../pages/staff/welcome.php");
-    }
-    else {
-      header("location: ../pages/login.php?q=invalid");
-    }
-  }
-  else {
-      header("location: ../pages/login.php?q=invalid");
-  }
-}
-
 else if($id) {
   $sql = "SELECT ad_uname,ad_pass FROM admin WHERE ad_uname = '$id';";
   $data = mysqli_query($db, $sql);
