eece822399c6d3aea694b8c0d6b88e8050593a6f
imranhei <imranhossainrana.2014@gmail.com>
2021-01-15 16:03:51 +0600

    css issue

--- pages/student/meal_menu.php
+++ pages/student/meal_menu.php
@@ -9,28 +9,30 @@
     <div>
       <?php include "std_nav.php" ?>
     </div>
+    <div class="">
+      <div class="output">
+          <h1>This is Meal Menu page</h1>
+          <?php
+          include_once '../../db/db.php';
 
-    <div class="output">
-        <h1>This is Meal Menu page</h1>
-        <?php
-        include_once '../../db/db.php';
 
+          $sql = "SELECT * FROM menu";
+          $result = mysqli_query($db, $sql);
 
-        $sql = "SELECT * FROM menu";
-        $result = mysqli_query($db, $sql);
+          echo "<table>"; // start a table tag in the HTML
 
-        echo "<table>"; // start a table tag in the HTML
+          while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
+          echo "<tr><td>" . $row['day'] . "</td><td>" . $row['breakfast_item'] . "</td><td>" . $row['lunch_item'] . "</td><td>" . $row['dinner_item'] . "</td></tr>";  //$row['index'] the index here is a field name
+          }
 
-        while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-        echo "<tr><td>" . $row['day'] . "</td><td>" . $row['breakfast_item'] . "</td><td>" . $row['lunch_item'] . "</td><td>" . $row['dinner_item'] . "</td></tr>";  //$row['index'] the index here is a field name
-        }
+          echo "</table>"; //Close the table in HTML
 
-        echo "</table>"; //Close the table in HTML
+          mysqli_close($db);
 
-        mysqli_close($db);
-
-        ?>
+          ?>
+      </div>
     </div>
+
 
 </body>
 </html>
